20601251d4030a687813e752996e0ab9f72ded52
kalpanabanjare <kalpanabanjare99@gmail.com>
2022-02-23 13:03:54 +0530

    Added: Attendance Logic

--- emp_wage_computation.sh
+++ emp_wage_computation.sh
@@ -1,1 +1,12 @@
 echo "Welcome to Employee Wage Computation Program"
+
+#Check Employee is Present or Absent
+
+emp_attendance=$(( RANDOM%2 ))
+
+if (( $emp_attendance == 1 ))
+then
+     echo "Present"
+else
+     echo "Absent"
+fi
